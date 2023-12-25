ab6c50c611fd8443d0a8a1895af841bc061ea0a9
Xyzuka <yeebrandon@outlook.com>
2023-12-25 12:04:52 -0800

    tailwind: using flexbox
    hidding the username on mobiles

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Brandon</div>;
+  return <div className="hidden text-sm font-semibold md:block">Brandon</div>;
 }
 
 export default Username;

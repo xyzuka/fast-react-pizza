52cacc0ab8ac983cece54e5c5d30006732ad051f
Xyzuka <yeebrandon@outlook.com>
2023-12-27 23:29:02 -0800

    tailwind: styling the menu
    resizing the menu images, placing text on the right side
    
    using the divide class to create spacing between the child elements inside the Menu component, adding some padding
    
    pushing the price to the bottom of the container - making the parent container a flex container (using mt-auto will push the price to the end)
    
    text formatting
    
    creating a button to add a menu item to the cart
    
    creating custom styles for the button based on the types prop passed into the Button component

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,18 +1,32 @@
 /* eslint-disable react/prop-types */
-import { formatCurrency } from '../../utilities/helpers';
+import { formatCurrency } from "../../utilities/helpers";
+import Button from "../../ui/Button";
 
 function MenuItem({ pizza }) {
   // eslint-disable-next-line no-unused-vars
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
-    <li>
-      <img src={imageUrl} alt={name} />
-      <div>
-        <p>{name}</p>
-        <p>{ingredients.join(', ')}</p>
-        <div>
-          {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
+    <li className="flex gap-4 py-2">
+      <img
+        src={imageUrl}
+        alt={name}
+        className={`${soldOut ? "opacity-70 grayscale" : ""} h-24`}
+      />
+      <div className="flex flex-grow flex-col pt-0.5">
+        <p className="font-medium">{name}</p>
+        <p className="text-sm capitalize italic text-stone-500">
+          {ingredients.join(", ")}
+        </p>
+        <div className="mt-auto flex items-center justify-between">
+          {!soldOut ? (
+            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+          ) : (
+            <p className="text-sm font-medium uppercase text-stone-500">
+              Sold out
+            </p>
+          )}
+          <Button type="small">Add to cart</Button>
         </div>
       </div>
     </li>
